f31666b08e21e432ae968ba1efdab97b15fd6c35
Katya Kirpicheva <kirpicheva203@gmail.com>
2025-11-29 18:01:10 +0300

    Adding class

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class test {
+    public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+    }
+
+    public static String getCallerClassAndMethodName() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        if (stackTrace.length >= 4) {
+            StackTraceElement caller = stackTrace[3];
+            return caller.getClassName() + "#" + caller.getMethodName();
+        } else {
+            // Точка входа (например, вызов из main или напрямую из JVM)
+            return null;
+        }
+    }
 }
